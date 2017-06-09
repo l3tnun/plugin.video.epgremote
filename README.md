2edf770c12557d866aafd56e7c1797fa80dfb10f
l3tnun <l3tnun@gmail.com>
2017-06-09 19:48:33 +0900

    add files

---
+++ README.md
@@ -0,0 +1,28 @@
+plugin.video.epgremote
+===
+
+EPGRemote for kodi
+
+## これはなに？
+
+[EPGRemote](https://github.com/l3tnun/EPGRemote) を使用して epgrec UNA の録画番組を kodi で視聴するためのプラグインです。
+
+## インストール方法
+zip でこのリポジトリをダウンロードして kodi で追加してください。
+
+## EPGRemote の設定
+
+`config.json` の `epgrecConfig` の中の `openHost` と `rootPath` の設定を忘れないでください。
+
+`openHost` は epgrec UNA のアドレスを設定してください。
+
+## 設定の注意
+
+設定の EPGRemote url の部分は下記のように入力してください。
+
+`http://192.168.0.2:8888`
+
+## その他
+
+ナビゲーションの昇順, 降順の設定は動かないです。
+なにかバグがあればお知らせください。
---
+++ main.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import xbmcgui
+import xbmcplugin
+import xbmcaddon
+import simplejson as json
+import urllib2
+import urlparse
+from urlparse import urljoin
+
+addon_handle = int(sys.argv[1])
+settings = xbmcaddon.Addon('plugin.video.epgremote')
+args = urlparse.parse_qs(sys.argv[2][1:])
+
+xbmcplugin.setContent(addon_handle, 'movies')
+
+mode = args.get('mode', None)
+
+def addList(video):
+    li = xbmcgui.ListItem(video['title'])
+
+    li.setIconImage(video['thumbnail'])
+    li.setThumbnailImage(video['thumbnail'])
+    li.setArt({'poster': video['thumbnail'], 'fanart': video['thumbnail'], 'landscape': video['thumbnail'], 'thumb': video['thumbnail'] })
+
+    li.setInfo('video', {
+        'plot': video['detail'],
+        'duration': video['duration'],
+    })
+
+    li.addContextMenuItems([ ('削除', 'RunScript(%s/delete.py, %d, %s)' % (settings.getAddonInfo('path'), video['id'], video['title'])) ])
+
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=video['url'], listitem=li)
+
+if __name__ == '__main__':
+    print 'aiueo'
+    print mode
+
+    server_url = settings.getSetting('server_url')
+    recorded_length = settings.getSetting('recorded_length')
+
+    if not server_url:
+        settings.openSettings()
+        server_url = settings.getSetting('server_url')
+
+    response = urllib2.urlopen(urljoin(server_url, '/api/kodi/recorded?length=' + str(recorded_length)))
+    strjson = response.read()
+    videos = json.loads(strjson)
+
+    for video in videos:
+        addList(video)
+
+    xbmcplugin.endOfDirectory(addon_handle)
+
